02e16253d1a17d264b9c99b25f03a977f73e2ab4
Shumi2010 <sneveld@rambler.ru>
2012-09-01 21:17:38 +0400

    x

--- src/Acme/ShowStuff/Controller/AdminBoardController.php
+++ src/Acme/ShowStuff/Controller/AdminBoardController.php
@@ -6,6 +6,7 @@ namespace Acme\ShowStuff\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Acme\ShowStuff\Form\EditBoardForm;
 
 
 class AdminBoardController extends Controller
@@ -13,9 +14,28 @@ class AdminBoardController extends Controller
       public function indexAction()
       {
 
-         return $this->render('AcmeShowStuff:AdminBoard:adminboard.html.twig', array('title_name'=>'Start List Name', 'txt'=>'Start List Text'));
+         $repository = $this->getDoctrine()->getRepository('AcmeShowStuff:Companies');
+         $em = $this->getDoctrine()->getEntityManager();
+         $compan = $repository->findAll();
+         //$compan = $repository->find('0');
+         //$query = $em->createQuery('SELECT shortName FROM AcmeShowStuff:Companies');
+         //$compan = $query->getResult();  
+
+         return $this->render('AcmeShowStuff:AdminBoard:adminboard.html.twig', array('title_name'=>'Page Title', 'txt'=>'Page Text', 'companies'=>$compan));
       }
 
+      public function editAction($num)
+      {
+         $repository = $this->getDoctrine()->getRepository('AcmeShowStuff:Companies');
+         $em = $this->getDoctrine()->getEntityManager();
+         $compan = $repository->find($num);
+
+         $form = $this->createForm(new EditBoardForm(), $compan);
+         $request = $this -> getRequest();
+          
+         return $this->render('AcmeShowStuff:AdminBoard:adminboardedit.html.twig', array('title_name'=>'AdminBoard Redactor', 'txt'=>'Page Text Edit', 'companies'=>$compan, 'form'=>$form->createView()));
+      }
+     
 }
 
 
